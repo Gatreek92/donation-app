632f640412f66500bcd8f4b12982025694232550
hamdi <hamdigatri92@gmail.com>
2018-12-07 23:16:22 +0100

    Create variables and constants.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 'use strict'
-window.onload = () => (
- 
-)
+window.onload = () => {
+    const total = 1000;
+    const dealine = 01-01-2019;
+    let donation = 0;
+    let donors = 0;
+    let left = total - donation;
+}
