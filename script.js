0e170992cd570a5ee9d4474a1d06dc4b5347e10f
hamdi <hamdigatri92@gmail.com>
2018-12-09 19:46:19 +0100

    calculate days left.

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 'use strict'
 window.onload = () => {
     const total = 1000;
-    const dealine = 01-01-2019;
+const dealine = new Date(2019,0,1);
+const currentDay = new Date() ;
     let donation = 0;
     let donors = 0;
-    let left = total - donation;
+let amountLeft = total - donation;
+let daysLeft;
+
+window.onload = () => {
+    daysDiff(dealine,currentDay);
+    updateCard();
+
+}
+
+const daysDiff = (date1,date2) => {
+   let date1_ms =  date1.getTime();
+   let date2_ms =  date2.getTime();
+   let one_day_ms = 1000*60*60*24; // one day in ms
+
+   let diff =  date1_ms - date2_ms;
+   daysLeft = Math.round(diff/one_day_ms);
 }
